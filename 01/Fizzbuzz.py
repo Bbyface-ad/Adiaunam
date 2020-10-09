873e3b2f8755b2d5c6bf691864c2cd4dcbefd323
Bbyface-ad <71518350+Bbyface-ad@users.noreply.github.com>
2020-10-09 09:58:43 -0700

    Add files via upload
    I added a python file here

---
+++ 01/Fizzbuzz.py
@@ -0,0 +1,20 @@
+for x in range (1, 1000):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+
+    elif x % 3 == 0:
+        print("Fizz")
+    
+    
+    elif x % 5 == 0:
+        print("Buzz")
+    
+
+    else:
+        print(x)
+    
+
+
+
+
+
